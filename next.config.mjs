4d4a348e3bf8d711210a5b87274db6f15fcdd494
apathriel <gabrielandersen1@gmail.com>
2024-12-06 05:47:36 +0100

    Add placehodler charts + map

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "mapbox-gl": "mapbox-gl/dist/mapbox-gl.js",
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
--- src/app/components/Viewer/Homescreen.js
+++ src/app/components/Viewer/Homescreen.js
@@ -1,8 +1,51 @@
 import React from "react";
 import styles from "./documentViewer.module.css";
+import dynamic from "next/dynamic";
+const DynamicMap = dynamic(() => import("./Map"), {
+  ssr: false,
+  loading: () => <p>Loading map...</p>,
+});
+
+import { Area, AreaChart, CartesianGrid, XAxis, Bar, BarChart } from "recharts";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+
+const chartData = [
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "June", desktop: 214, mobile: 140 },
+];
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "hsl(var(--chart-1))",
+  },
+  mobile: {
+    label: "Mobile",
+    color: "hsl(var(--chart-2))",
+  },
+};
 
 const HomeScreen = ({ documents, setSelectedItem }) => {
-  const rootFolders = documents.filter(doc => doc.type === 'folder' && doc.parent_id === null);
+  const rootFolders = documents.filter(
+    (doc) => doc.type === "folder" && doc.parent_id === null
+  );
 
   const handleButtonClick = (doc) => {
     setSelectedItem(doc);
@@ -13,15 +56,96 @@ const HomeScreen = ({ documents, setSelectedItem }) => {
       <div className={styles.bentoBox}>
         <div className={styles.leftColumn}>
           <div className={styles.box}>
-            <img src="/simple_line.png" alt="Simple Line" />
+            <Card className="h-full">
+              <CardHeader>
+                <CardTitle>Area Chart - Stacked</CardTitle>
+                <CardDescription>
+                  Showing total visitors for the last 6 months
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="h-72 p-4">
+                <ChartContainer className="h-full" config={chartConfig}>
+                  <AreaChart
+                    accessibilityLayer
+                    data={chartData}
+                    margin={{
+                      left: 0,
+                      right: 0,
+                    }}
+                  >
+                    <CartesianGrid vertical={false} />
+                    <XAxis
+                      dataKey="month"
+                      tickLine={false}
+                      axisLine={false}
+                      tickMargin={8}
+                      tickFormatter={(value) => value.slice(0, 3)}
+                    />
+                    <ChartTooltip
+                      cursor={false}
+                      content={<ChartTooltipContent indicator="dot" />}
+                    />
+                    <Area
+                      dataKey="mobile"
+                      type="natural"
+                      fill="var(--color-mobile)"
+                      fillOpacity={0.4}
+                      stroke="var(--color-mobile)"
+                      stackId="a"
+                    />
+                    <Area
+                      dataKey="desktop"
+                      type="natural"
+                      fill="var(--color-desktop)"
+                      fillOpacity={0.4}
+                      stroke="var(--color-desktop)"
+                      stackId="a"
+                    />
+                  </AreaChart>
+                </ChartContainer>
+              </CardContent>
+            </Card>
           </div>
           <div className={styles.box}>
-            <img src="/simple_bar.png" alt="Simple Bar" />
+            <Card className="h-full">
+              <CardHeader>
+                <CardTitle>Bar Chart - Multiple</CardTitle>
+                <CardDescription>January - June 2024</CardDescription>
+              </CardHeader>
+              <CardContent className="h-72 p-4">
+                <ChartContainer className="h-full" config={chartConfig}>
+                  <BarChart accessibilityLayer data={chartData}>
+                    <CartesianGrid vertical={false} />
+                    <XAxis
+                      dataKey="month"
+                      tickLine={false}
+                      tickMargin={10}
+                      axisLine={false}
+                      tickFormatter={(value) => value.slice(0, 3)}
+                    />
+                    <ChartTooltip
+                      cursor={false}
+                      content={<ChartTooltipContent indicator="dashed" />}
+                    />
+                    <Bar
+                      dataKey="desktop"
+                      fill="var(--color-desktop)"
+                      radius={4}
+                    />
+                    <Bar
+                      dataKey="mobile"
+                      fill="var(--color-mobile)"
+                      radius={4}
+                    />
+                  </BarChart>
+                </ChartContainer>
+              </CardContent>
+            </Card>
           </div>
         </div>
         <div className={styles.rightColumn}>
           <div className={styles.box}>
-            <img src="/aarhus_kort.jpg" alt="Aarhus Kort" />
+            <DynamicMap latitude={56.1156354} longitude={10.078475} zoom={13} />
           </div>
         </div>
       </div>
